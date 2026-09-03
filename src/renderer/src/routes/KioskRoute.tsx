8e03896db7f775be3a64850956237e54d1d09b0e
Nirav <inirav88@gmail.com>
2026-09-03 11:38:20 +0530

    feat(kiosk): wrap presentation kiosk routes with StartupLockGate

--- src/renderer/src/routes/KioskRoute.tsx
+++ src/renderer/src/routes/KioskRoute.tsx
@@ -3,11 +3,13 @@ import ProjectLauncher from '../pages/kiosk/ProjectLauncher'
 import ProjectShowcase from '../pages/kiosk/ProjectShowcase'
 import PresenterConsole from '../pages/kiosk/PresenterConsole'
 
+import { StartupLockGate } from '../components/kiosk/StartupLockGate'
+
 export default function KioskRoute(): JSX.Element {
   return (
     <Routes>
-      <Route index element={<ProjectLauncher />} />
-      <Route path="project/:projectId" element={<ProjectShowcase />} />
+      <Route index element={<StartupLockGate><ProjectLauncher /></StartupLockGate>} />
+      <Route path="project/:projectId" element={<StartupLockGate><ProjectShowcase /></StartupLockGate>} />
       <Route path="presenter/:projectId" element={<PresenterConsole />} />
     </Routes>
   )
